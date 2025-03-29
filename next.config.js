98df2fef183f6770bdb92a7e375078f9d48f4ae1
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2025-03-29 10:54:07 +0500

    resolved typescript error

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Absolute path to your Strapi admin directory
-    const strapiAdminPath = require("path").resolve(
-      __dirname,
-      "demo-strapi/src/admin"
-    );
-
-    // Ignore the entire Strapi admin directory
-    config.plugins.push(
-      new (require("webpack").IgnorePlugin)({
-        resourceRegExp: new RegExp(
-          `^${strapiAdminPath.replace(/\\/g, "\\\\")}`
-        ),
-      })
-    );
-    return config;
+module.exports = {
+  output: "standalone", // Isolates dependencies
+  experimental: {
+    outputFileTracingExcludes: {
+      "**/*": ["**/demo-strapi/**", "**/@strapi/**"],
+    },
   },
 };
-
-// Use module.exports for CommonJS compatibility
-module.exports = nextConfig;
