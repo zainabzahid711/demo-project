10b65b1f669a4fec81b9683c38d931a73d388ccb
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2025-03-29 09:12:19 +0500

    fixed build errorr

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+  // Add this webpack config to explicitly exclude Strapi
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /demo-strapi[\\/]src[\\/]admin/,
+      loader: "ignore-loader",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
